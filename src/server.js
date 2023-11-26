395d9fed0462010a93e13376e02f283b1b478e34
Mattias <106891644+Neniuk@users.noreply.github.com>
2023-11-26 18:03:57 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -47,20 +47,20 @@ app.get("*", (req, res) => {
 // app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-// 	next(createError(404));
-// });
+app.use(function (req, res, next) {
+	next(createError(404));
+});
 
 // error handler
-// app.use((err, req, res, next) => {
-// 	// set locals, only providing error in development
-// 	res.locals.message = err.message;
-// 	res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use((err, req, res, next) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-// 	// render the error page
-// 	res.status(err.status || 500);
-// 	res.send("error");
-// });
+	// render the error page
+	res.status(err.status || 500);
+	res.send("error");
+});
 
 // Socket.io
 let connectedUsers = new Set();
