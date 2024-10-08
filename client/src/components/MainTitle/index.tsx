c4b56b45b44f4f03a27b3edda291b4a7171d86c8
Neniuk <106891644+Neniuk@users.noreply.github.com>
2024-10-08 18:47:25 +0300

    img loading

--- client/src/components/MainTitle/index.tsx
+++ client/src/components/MainTitle/index.tsx
@@ -1,25 +1,45 @@
+import React, { useEffect, useState } from "react";
+
 import mainTitleDecorationBlue from "../../assets/main-title-decoration-blue.png";
 
-const MainTitle = () => (
-    <div className="mb-0 mt-6 flex w-[95%] flex-row items-center justify-center gap-8 overflow-hidden md:mb-6 md:w-full">
-        <img
-            className="main-title-decoration h-[32px] w-auto max-w-full flex-shrink-0 select-none object-contain brightness-50"
-            src={mainTitleDecorationBlue}
-            alt="Main title decoration"
-            width="320"
-            height="32"
-        />
-        <h3 className="text-titleColorSecondary text-center text-3xl">
-            NENIUK.DEV
-        </h3>
-        <img
-            className="main-title-decoration h-[32px] w-auto max-w-full flex-shrink-0 rotate-180 select-none object-contain brightness-50"
-            src={mainTitleDecorationBlue}
-            alt="Main title decoration"
-            width="320"
-            height="32"
-        />
-    </div>
-);
+const MainTitle: React.FC = () => {
+    const [mainTitleIsLoading, setMainTitleIsLoading] = useState(true);
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = mainTitleDecorationBlue;
+        img.onload = () => setMainTitleIsLoading(false);
+    }, []);
+
+    return (
+        <div className="mb-0 mt-6 flex w-[95%] flex-row items-center justify-center gap-8 overflow-hidden md:mb-6 md:w-full">
+            {mainTitleIsLoading ? (
+                <div className="h-[32px] w-[320px]"></div>
+            ) : (
+                <img
+                    className="main-title-decoration h-[32px] w-auto max-w-full flex-shrink-0 select-none object-contain brightness-50"
+                    src={mainTitleDecorationBlue}
+                    alt="Main title decoration"
+                    width="320"
+                    height="32"
+                />
+            )}
+            <h3 className="text-titleColorSecondary text-center text-3xl">
+                NENIUK.DEV
+            </h3>
+            {mainTitleIsLoading ? (
+                <div className="h-[32px] w-[320px]"></div>
+            ) : (
+                <img
+                    className="main-title-decoration h-[32px] w-auto max-w-full flex-shrink-0 rotate-180 select-none object-contain brightness-50"
+                    src={mainTitleDecorationBlue}
+                    alt="Main title decoration"
+                    width="320"
+                    height="32"
+                />
+            )}
+        </div>
+    );
+};
 
 export default MainTitle;
